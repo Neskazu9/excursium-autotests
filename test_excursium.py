9f4c6ede381e38b33cead56072a556443469ffff
Neskazu9 <kewr777@mail.ru>
2026-09-19 16:13:44 +0300

    Add test for large group option display

--- test_excursium.py
+++ test_excursium.py
@@ -138,3 +138,13 @@ def test_whatsapp_contact_is_displayed(driver):
         "xpath",
         "//*[contains(normalize-space(.), 'Написать в WhatsApp')]"
     ).is_displayed()
+
+def test_large_group_option_is_displayed(driver):
+    driver.get(
+        "https://excursium.com/ekskursii-dlya-shkolnikov/list"
+    )
+
+    assert driver.find_element(
+        "xpath",
+        "//*[contains(normalize-space(.), '45 +')]"
+    ).is_displayed()
