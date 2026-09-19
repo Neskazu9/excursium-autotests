f15de0fcf2e31ba8a3f2df9500d3c2f1b504cc93
Neskazu9 <kewr777@mail.ru>
2026-09-19 16:31:21 +0300

    Delete test_search_button_is_displayed function
    Removed test for search button visibility in excursions page.

--- test_excursium.py
+++ test_excursium.py
@@ -139,11 +139,3 @@ def test_whatsapp_contact_is_displayed(driver):
         "//*[contains(normalize-space(.), 'Написать в WhatsApp')]"
     ).is_displayed()
 
-def test_search_button_is_displayed(driver):
-    page = ExcursionsPage(driver)
-    page.open()
-
-    assert driver.find_element(
-        "xpath",
-        "//*[self::button or self::a][contains(normalize-space(.), 'Найти')]"
-    ).is_displayed()
